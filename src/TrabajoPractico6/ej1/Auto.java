da8ba4d8c3bbfd7ac7200efab61c5f93df8a313b
joacomedel <joaquinmedel1234@gmail.com>
2022-10-27 16:11:32 -0300

    autos2

--- src/TrabajoPractico6/ej1/Auto.java
+++ src/TrabajoPractico6/ej1/Auto.java
@@ -1,10 +1,26 @@
 package TrabajoPractico6.ej1;
 public class Auto extends Thread{
     char tipo;
-    public Auto (char tipo){
+    int id;
+    GestionaTrafico gestionaTrafico;
+    public Auto (char tipo , GestionaTrafico gestionaTrafico, int id){
         this.tipo = tipo;
+        this.gestionaTrafico = gestionaTrafico;
+        this.id = id;
     }
     public void run() {
-        switch()
+        cruzar();
+    }
+    public void cruzar(){
+        switch (tipo) {
+            case 'S':
+                gestionaTrafico.entrarAutoSur(id);
+                gestionaTrafico.salirAutoNorte(id);
+                break;
+            case 'N':
+                gestionaTrafico.entrarAutoNorte(id);
+                gestionaTrafico.salirAutoSur(id);
+                break;
+        }
     }
 }
--- src/TrabajoPractico6/ej1/GestionaTrafico.java
+++ src/TrabajoPractico6/ej1/GestionaTrafico.java
@@ -1,4 +1,48 @@
 package TrabajoPractico6.ej1;
-public class GestionaTrafico{
+import java.util.Random;
 
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+
+import EstructurasThreadSafe.*;
+public class GestionaTrafico{
+    Buffer<Integer> bufferNorte;
+    Buffer<Integer> bufferSur;
+    int autosCruzaron;
+    boolean norteCruzan;
+    int autosMax = 10;
+    int cantMinima = 1;
+    public GestionaTrafico(){
+        bufferNorte = new Buffer<Integer>();
+        bufferSur = new Buffer<Integer>();
+        Random random = new Random();
+        norteCruzan = random.nextBoolean();
+    }
+    public void entrarAutoSur(int id){
+        bufferSur.add(id);
+        System.out.println(id + "entra por el sur");
+    }
+    public void salirAutoNorte(int id) throws InterruptedException{
+        boolean puedenCruzar = false;
+        while (!puedenCruzar) {
+            bufferSur.wait();
+            if (!norteCruzan) {
+                if (bufferSur.peek() == id) {
+                    bufferSur.poll();
+                    System.out.println(id + "cruzando hacia el norte");
+                    autosCruzaron++;
+                    
+                    if (autosCruzaron >= autosMax && bufferSur.size() >= cantMinima)norteCruzan = true;
+                }
+            }
+            
+        }
+    }
+    public void entrarAutoNorte(int id){
+        bufferNorte.add(id);
+    }
+    public void salirAutoSur(int id){
+
+    }
+    
+    
 }
