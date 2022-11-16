e36141df5b9d6077225197fcd39736ed0d26193b
joacomedel <joaquinmedel1234@gmail.com>
2022-11-16 12:56:34 -0300

    AvionesDeMierdaNoAndan

--- src/TrabajoPractico5/ej5/Aereopuerto.java
+++ src/TrabajoPractico5/ej5/Aereopuerto.java
@@ -2,24 +2,30 @@ package TrabajoPractico5.ej5;
 
 import java.util.concurrent.Semaphore;
 
-import javax.swing.plaf.synth.SynthMenuItemUI;
+
 
 public class Aereopuerto {
     Semaphore semMutex;
-    //Semaphore semPista;
+    Semaphore semPista = new Semaphore(1);
     Semaphore semPistaAterrizar = new Semaphore(1);
     Semaphore semPistaDespegue = new Semaphore(1);
-    int cantAterrizaron;
-    int cantDespegaron;
+    int cantAterrizaron = 0;
+    int colaDespegue = 0;
+    int colaAterrizaje = 0;
     boolean aterizarPrioridad = true;
     int tiempoTarea = 15;
+    int estado = 1; // Estado 1 , Cualquiera pasa , Estado 2 Despegue tiene prioridad , Estado 3
+                    // aterrizar tiene prioridad
 
     public void despegar() throws InterruptedException {
-        semPistaDespegue.acquire();
+        
+    }
+    public void aterrizar() throws InterruptedException {
+        
+    }
+    public void aterrizarTerminar(){
         
 
     }
-    public void aterrizar() throws InterruptedException{
 
-    }
 }
